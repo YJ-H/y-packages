1f2f5b19b606c92b79bdd283435931f2c03198a1
YJ-H <a910102423@gmail.com>
2020-07-09 23:26:52 +0800

    update

--- packages/@y-pkgs/cli/lib/cli.js
+++ packages/@y-pkgs/cli/lib/cli.js
@@ -1,8 +1,31 @@
 'use strict';
+const path = require('path')
+const fs = require('fs')
+const inquirer = require('inquirer')
+const ejs = require('ejs')
+async function cli() {
+    // TODO
+	let anwsers = await inquirer.prompt([
+		{
+			 type: 'input',
+			 name: 'name',
+			 message: '项目名称?'
+		}
+	])
+	const tempDir = path.join(__dirname, 'templates')
+	const destDir = process.cwd()
+	fs.readdir(tempDir,(err, files) => {
+		if (err) throw err
+
+		files.forEach(file => {
+			ejs.renderFile(path.join(tempDir, file),anwsers, (err,result) => {
+				if (err) throw err
+				fs.writeFileSync(path.join(destDir,file), result)
+			})
+
+		})
+
+	})
+}
 
 module.exports = cli;
-
-function cli() {
-    // TODO
-    console.log(1111)
-}
